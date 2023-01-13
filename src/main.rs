d13cada535349554fac24fee180c4831395f9c07
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2023-01-12 22:06:36 -0500

    added support for quit command

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
 use std::{
     collections::VecDeque,
     str::FromStr,
-    sync::{mpsc::Sender, Arc, Mutex},
+    sync::{mpsc::Sender, Arc, Mutex}
 };
 
 use yea_fen::{chess_engines::minimax, moves::MoveType, Colored, GameState, Piece, Pos};
-
+fn make_info(info: &str) -> String {
+    info.lines().map(|s| format!("# info {s}\n")).collect::<String>().trim().to_owned()
+}
 fn main() {
     // wait until the "uci" command is received
     let mut input = String::new();
     // let mut done = String::new();
     let mut gs = GameState::new();
-    // let mut best_move: Option<(MoveType<Pos, Colored<Piece>>, Option<Colored<Piece>>)> = None;
+    let mut best_move: Option<(String)> = None;
     // let (tx_go, rx_go) = std::sync::mpsc::channel::<Option<(MoveType<Pos, Colored<Piece>>, Option<Colored<Piece>>)>>();
     // let (tx_set, rx_set) = std::sync::mpsc::channel();
     // let mut set = false;
     // let mut go = false;
     let res = std::io::stdin();
-    let queue = Arc::new(Mutex::new(VecDeque::new()));
+    let queue: Arc<Mutex<VecDeque<String>>> = Arc::new(Mutex::new(VecDeque::new()));
     let queue2 = Arc::clone(&queue);
     std::thread::spawn(move || {
+        let mut move_list: Vec<String> = Vec::new();
         loop {
             let input: String = if let Ok(mut q) = queue2.lock() {
                 if let Some(i) = q.pop_front() {
-                    println!("info pop {i}");
+                    println!("{}", make_info(&i));
                     i
                 } else {
                     continue;
@@ -40,8 +43,22 @@ fn main() {
                 println!("readyok");
             } else if input.trim() == "ucinewgame" {
                 gs = NEW_STATE;
-            } else if input.trim().starts_with("position") {
-                set_position(input.trim(), &mut gs);
+            } else if input.trim() == "stop" {
+                // if let Some(moves) = best_move.clone() {
+                //     println!("{}", moves);
+                // } else {
+                //     println!("# info error: no best move found");
+                // }
+            }
+            else if input.trim() == "quit" {
+                std::process::exit(0);
+            }
+            else if input.trim().starts_with("position") {
+                set_position(input.trim(), &mut move_list, &mut gs);
+                println!("# info set position");
+                println!("{}", make_info(format!("board: \n{}", gs.get_board().to_string()).as_str()));
+
+                println!("{}", make_info(format!("gs fen {}", gs.to_string()).as_str()));
             } else if input.trim().starts_with("go") {
                 if let Some((m, p)) = minimax::minimax(&mut gs, 3) {
                     let p = if let Some(p) = p {
@@ -59,20 +76,22 @@ fn main() {
                     } else {
                         None
                     };
+                    let moves = format!("bestmove {}{}{}",
+                    m.from().0,
+                    m.to(),
+                    match p {
+                        Some(p) => p,
+                        None => ' ',
+                    });
+                    best_move = Some(moves.clone());
                     println!(
-                        "bestmove {}{}{}",
-                        m.from().0,
-                        m.to(),
-                        match p {
-                            Some(p) => p,
-                            None => ' ',
-                        }
+                        "{moves}"
                     );
                 } else {
-                    println!("info error: could not find best move");
+                    println!("# info error: could not find best move");
                 }
             }
-            // done.push_str(&input);
+
         }
     });
     loop {
@@ -80,33 +99,34 @@ fn main() {
 
         res.read_line(&mut input).unwrap();
 
-        println!("info input: {}", input);
+        println!("{}", make_info(format!("input: {}", input).as_str()));
         queue.lock().unwrap().push_back(input.clone());
         // push(input.clone());
     }
 }
 const NEW_STATE: GameState = GameState::new();
-fn set_position(pos_str: &str, gs: &mut GameState) {
+fn set_position(pos_str: &str, current_moves: &mut Vec<String>, gs: &mut GameState) {
     // remove the "position " from the str
     if let Some(pos_str) = pos_str.strip_prefix("position ") {
         let mut split_str = pos_str.split(" ");
         if let Some(r#type) = split_str.next() {
-            println!("info type: '{}'", r#type);
+            println!("{}", make_info(format!("type {}", r#type).as_str()));
             match r#type {
                 "startpos" => {
                     *gs = NEW_STATE;
                     if let Some("moves") = split_str.next() {
                         // do nothing
                     } else {
-                        println!("info error \"moves\" not found after \"startpos\"");
-                        return;
+                        println!("# info error \"moves\" not found after \"startpos\"");
+                        // return;
                     }
                 }
                 "fen" => {
                     let mut fen = String::new();
                     while let Some(s) = split_str.next() {
                         if s == "moves" {
-                            println!("info fen: {}", fen);
+                            println!("{}", make_info(&fen));
+                            println!("{}", make_info(&format!("fen: {}", fen)));
                             break;
                         }
                         fen.push_str(s);
@@ -116,22 +136,42 @@ fn set_position(pos_str: &str, gs: &mut GameState) {
                     *gs = match GameState::from_str(&fen) {
                         Ok(new_gs) => new_gs,
                         Err(_) => {
-                            println!("info error invalid fen");
+                            println!("# info error invalid fen");
                             return;
                         }
                     }
                 }
                 _ => {
-                    println!("info error invalid position type");
+                    println!("# info error invalid position type");
                     return;
                 }
             }
+            // let new_moves = split_str.clone().collect::<Vec<&str>>();
+            // let mut move_iter = current_moves.iter();
+            // let mut new_move_iter = split_str.clone();
+            // loop {
+            //     let nxt_new = new_move_iter.next();
+            //     let nxt_old = move_iter.next();
+            //     if let (Some(new_mover), Some(old_mover)) = (nxt_new, nxt_old) {
+            //         if new_mover != old_mover {
+            //             break;
+            //         }
+            //     } else if nxt_new.is_some() {
+            //         split_str = new_move_iter.clone();
+            //         break;
+            //     } 
+            //     else {
+            //         break;
+            //     }
+            // }
             for r#move in split_str {
                 if let Some(m) = get_move(r#move, gs) {
                     if gs.do_move(m.0, m.1) {
-                        println!("info move {} done", r#move);
+                        // println!("# info move {} done", r#move);
+                        println!("{}", make_info(format!("move {} done", r#move).as_str()));
                     } else {
-                        println!("info error move {} failed", r#move);
+                        // println!("# info error move {} failed", r#move);
+                        println!("{}", make_info(format!("move {} failed", r#move).as_str()));
                     }
                 }
             }
@@ -153,7 +193,7 @@ fn thread_find_move(
 fn thread_set_move(gs: GameState, tx: Sender<GameState>, pos_str: String) {
     let mut gs = gs.clone();
     std::thread::spawn(move || {
-        set_position(&pos_str, &mut gs);
+        set_position(&pos_str, &mut vec![], &mut gs);
         println!("position set thread");
         tx.send(gs).unwrap();
     });
